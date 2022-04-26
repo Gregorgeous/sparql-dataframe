fb0f97dd18577393ee02a6404d6f70b9b0c6ab21
Greg Rybak <g.r.fisher.pl@gmail.com>
2022-04-26 21:11:07 +0200

    change setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ def readme():
 
 setup(
     name='sparql_dataframe',
-    version='0.4',
+    version='0.4-fork',
     description='Convert SPARQL results to Pandas dataframes',
     long_description=readme(),
     long_description_content_type='text/markdown',
-    url='https://github.com/lawlesst/sparql-dataframe',
+    url='https://github.com/Gregorgeous/sparql-dataframe',
     author='Ted Lawless',
     author_email='lawlesst@gmail.com',
     license='MIT',
-    packages=['sparql_dataframe'],
+    packages=['sparql_dataframe_fork'],
     install_requires=[
     'pandas>=0.22.0',
     'SPARQLWrapper>=1.8.1'
